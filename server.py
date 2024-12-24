beb1f59dfd677bf30270bd658df435662e6d2851
malikkhubiev <65446220+malikkhubiev@users.noreply.github.com>
2024-12-24 11:09:20 +0300

    ;klk

--- server.py
+++ server.py
@@ -896,11 +896,20 @@ async def getMyMoney(request: Request, db: Session = Depends(get_db)):
     
 @app.get("/offer")
 async def get_offer():
-    pdf_path = "offer.pdf"  # Путь к вашему PDF-файлу
+    pdf_path = "offer.pdf"
     with open(pdf_path, "rb") as file:
         pdf_data = file.read()
     return Response(content=pdf_data, media_type="application/pdf", headers={
         "Content-Disposition": "inline; filename=offer.pdf"
+    })
+    
+@app.get("/privacy")
+async def get_privacy():
+    pdf_path = "privacy.pdf"  # Путь к вашему PDF-файлу
+    with open(pdf_path, "rb") as file:
+        pdf_data = file.read()
+    return Response(content=pdf_data, media_type="application/pdf", headers={
+        "Content-Disposition": "inline; filename=privacy.pdf"
     })
 
 # Database session dependency
