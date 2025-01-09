06c7da0b02b56109f9c3dd93510bd9552eb74bb5
Malik Khubiev <65446220+malikkhubiev@users.noreply.github.com>
2025-01-09 12:46:03 +0300

    Update base.py

--- api/base.py
+++ api/base.py
@@ -226,10 +226,9 @@ async def generate_clients_report(request: Request):
         logging.info("Referral details found.")  # Логируем только факт наличия рефералов
         for referral in referred_details:
             logging.debug(f"Processing referral: {referral}")  # Логируем только сам процесс обработки
+            attributes = vars(referral)
             attributes = {column.key: getattr(referral, column.key) for column in referral.__mapper__.column_attrs}
             logging.debug(f"Referral attributes: {attributes}")  # Логируем атрибуты реферала, если это нужно
-            
-            attributes = vars(referral)
             
             referred_user = await get_referred_user(referral.referred_id)
             
